3b915d4f643a7e3b711f48d41cd19d604d6f8859
Annie-mae Jenkins <annie-maejenkins@Annie-maes-MacBook-Pro.local>
2017-07-26 11:19:26 -0400

    fixed fetch with notes

--- src/components/App.js
+++ src/components/App.js
@@ -11,29 +11,32 @@ constructor(props){
   };
 }
 
-
+// whenever doing a fetch request, log the top level of the response to see if
+// you need to drill to the next level
+// first you are fetching info from the link and it returns a promise where the information is called whatever(i used result)
+// but result isn't information we can read so we have to use .json which returns another promise
+// we assign that json info the name response and then set the state of menu to the data in response
 componentDidMount(){
-  fetch('http://tiny-lasagna-server.herokuapp.com/collections/reactthaimenu')
-  .then(results => results.json())
-  .then(responseData => {
-    this.setState({menu: responseData.results})
-  })
-  .catch((error) => {
-    console.log("Error with Fetching : ", error);
+  fetch('http://tiny-lasagna-server.herokuapp.com/collections/reactthaimenu').then((result) => {
+    result.json().then((response) => {
+      this.setState({menu: response[0]});
+      console.log(this.state.menu.Entrees);
+    });
   });
 }
 
-
   render() {
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="fetchdata">
+
+        </div>
+
       </div>
     );
   }
