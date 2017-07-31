085823e49a85ee4cf6e8b391483e5c7645d3b7ae
Annie-mae Jenkins <annie-maejenkins@Annie-maes-MacBook-Pro.local>
2017-07-30 23:43:35 -0400

    components are showing correctly now

--- src/components/App.js
+++ src/components/App.js
@@ -7,39 +7,19 @@ import BaseLayout from './BaseLayout.js';
 class App extends Component {
 constructor(props){
   super(props);
-
-  this.state = {
-    menu: [],
-  };
-}
-
-// whenever doing a fetch request, log the top level of the response to see if
-// you need to drill to the next level
-// first you are fetching info from the link and it returns a promise where the information is called whatever(i used result)
-// but result isn't information we can read so we have to use .json which returns another promise
-// we assign that json info the name response and then set the state of menu to the data in response
-componentDidMount(){
-  fetch('http://tiny-lasagna-server.herokuapp.com/collections/reactthaimenu')
-  .then((result) => {
-    result.json().then((response) => {
-      this.setState({menu: response[0]});
-      console.log(this.state.menu.Entrees);
-    });
-  });
 }
 
   render() {
-
     return (
       <div className="App">
+
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
+
         <div className="fetchdata">
-
-        <div>{BaseLayout}</div>
-
+        <BaseLayout/>
         </div>
 
       </div>
--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -8,13 +8,31 @@ import Appetizer from './appetizer.js';
 export default class BaseLayout extends Component{
   constructor(props){
     super(props);
+    this.state = {
+      menu: [],
+    };
+  }
+
+  // whenever doing a fetch request, log the top level of the response to see if
+  // you need to drill to the next level
+  // first you are fetching info from the link and it returns a promise where the information is called whatever(i used result)
+  // but result isn't information we can read so we have to use .json which returns another promise
+  // we assign that json info the name response and then set the state of menu to the data in response
+  componentDidMount(){
+    fetch('http://tiny-lasagna-server.herokuapp.com/collections/reactthaimenu')
+    .then((result) => {
+      result.json().then((response) => {
+        this.setState({menu: response[0]});
+        console.log(this.state.menu.Entrees);
+      });
+    });
   }
   render(){
     return(
     <div>
-    <div>{Dessert}</div>
-    <div>{Appetizer}</div>
-    <div>{Entree}</div>
+    <Dessert/>
+    <Appetizer/>
+    <Entree/>
     </div>
     );
   }
